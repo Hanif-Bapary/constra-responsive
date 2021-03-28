3c535d21202afad88440fb1c58622518721bea48
Hanif Bapary <79474540+Hanif-Bapary@users.noreply.github.com>
2021-03-28 23:20:38 +0600

    Add files via upload

---
+++ js/customed.js
@@ -0,0 +1,76 @@
+$(function(){
+
+    $('.our-carousel , .testimonial').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow:1,
+        autoplay:true,
+        autoplaySpeed: 2000,
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
